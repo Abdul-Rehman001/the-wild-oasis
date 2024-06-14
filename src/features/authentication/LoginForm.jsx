23c700340d4aea04810bb00e8014cdb246cac1d1
Abdul Rehman Tahir <139611482+Abdul-Rehman001@users.noreply.github.com>
2024-06-15 00:55:09 +0530

    Update LoginForm.jsx

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,7 +8,7 @@ import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("abdul@gmail.com");
-  const [password, setPassword] = useState("abdul@123");
+  const [password, setPassword] = useState("abdul123");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
